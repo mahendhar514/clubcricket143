3fb3e6d3dd6490bfc3e85e07758e5c125cf351bc
mahendhar514 <124228828+mahendhar514@users.noreply.github.com>
2023-03-21 16:42:21 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -8,7 +8,7 @@ private DBConnection() {} static
 try {
 	Class.forName("com.mysql.cj.jdbc.Driver");
 	con=DriverManager.getConnection
-			("jdbc:mysql://65.1.132.148:3306/score","admin","12345678");
+			("jdbc:mysql://183.16.7.49:3306/score","admin","12345678");
 	}catch(Exception e) 
 	{e.printStackTrace();}
 	}//end of block
